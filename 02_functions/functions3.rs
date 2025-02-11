c02144ab8d43f14ffe01923dce19801760b5a7d5
nothing995 <a.k.02@hotmail.com>
2025-02-11 10:22:57 +0400

    Add files via upload

---
+++ 02_functions/functions3.rs
@@ -0,0 +1,10 @@
+fn call_me(num: u8) {
+    for i in 0..num {
+        println!("Ring! Call number {}", i + 1);
+    }
+}
+
+fn main() {
+    // TODO: Fix the function call.
+    call_me(9); // any number between 0 to 255
+}
